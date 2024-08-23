2ba4e23230a4016f5830580db6499f446b33c41f
imam160 <imambayhaqi114@gmail.com>
2024-08-23 16:10:38 +0700

    ubah meta

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <meta name="description" content="Selamat datang di halaman login. Masuk untuk mengakses akun Anda.">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
 
