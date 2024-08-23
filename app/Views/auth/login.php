308d351349a62c71493a766a55836e376a835f85
imam160 <imambayhaqi114@gmail.com>
2024-08-23 16:15:24 +0700

    update login

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <meta name="description" content="Selamat datang di halaman login. Masuk untuk mengakses akun Anda.">
+    <title>MONITORING TOR</title>
+    <meta name="description" content="Selamat datang di MONITORING TOR, platform untuk memantau dan mengelola TOR">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css">
 </head>
 
